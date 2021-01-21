f96ef948b8e7c87617deaa9b8e4fd6f20ea9f1c7
zhanglan1315 <delylaric@gmail.com>
2021-01-21 13:39:09 +0800

    feat(index): 调整首页标题内容

--- src/views/Index/TheHero.vue
+++ src/views/Index/TheHero.vue
@@ -3,15 +3,16 @@
     color="white" image="home/hero-1.jpg"
     style="height: 768px"
   >
-    <div style="width: 560px">
-      <h1 class="is-size-3 has-text-white">
-        {{$t("title")}}
-      </h1>
-      <div style="height: 0.75rem"></div>
-      <p class="has-text-white">
+    <div>
+    <h1 class="is-size-2 has-text-white">
+      {{$t("title")}}
+    </h1>
+    <div style="height: 0.75rem"></div>
+
+      <p class="is-size-5 has-text-white" style="width: 560px">
         {{$t("content")}}
       </p>
-      <div style="height: 0.75rem"></div>
+      <div style="height: 1rem"></div>
       <p>
         <VideoButton :text="$t('video')">
           <source src="https://img.als-yuchuan.com/videos/yuchuan-introduce" type="video/mp4" >
@@ -38,14 +39,16 @@ export default defineComponent({
   i18n: {
     messages: {
       [zhCN]: {
-        title: "智能物流专家",
+        title: "玉串自动化",
         video: "观看视频",
-        content: "智能物流是利用集成智能化技术，使物流系统能模仿人类的智能，具有思维、感知、学习、推理判断和自行解决物流中某些问题的能力",
+        content: "集全球前沿技术，成就智慧物流自动化系统！",
       },
       [enUS]: {
-        title: "Intelligent logistics expert",
+        title: "Yuchuan automation technology",
         video: "Video",
-        content: "Intelligent logistics using integrated intelligent technology make the logistics system can imitate human intelligence with thinking, perception, learning, reasoning and judgment and the ability to solve some problems in logistics.",
+        content: `
+          Yuchuan automation integrate globle leading technology to make intelligent logistics system.
+        `,
       }
     }
   }
